bf2e1ccc279cf92203dd0d817021f103c320175c
Juan Diego <jdvo.214@gmail.com>
2022-01-03 18:52:16 -0500

    Uploading Js IV Tests

---
+++ 05-JS-IV/homework/hwsupport.js
@@ -0,0 +1,16 @@
+//function agregarMetodoCalculoDescuento(producto) {
+    // Agregar un método (función) al objeto "producto" llamado "calcularPrecioDescuento"
+    // Este método debe multiplicar el "precio" del "producto" ("producto.precio" o "producto[precio]") y "porcentajeDeDescuento" para obtener el descuento
+    // El método resta el descuento del precio y devuelve el precio con descuento
+    // Devuelve el objeto "producto" al final de la función
+    // Ejemplo:
+    // producto.precio -> 20
+    // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
+    // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
+    // Tu código:
+    var producto = {
+        calcularPrecioDescuento: function () {
+            precioDescuento = this.precio - (this.precio * this.porcentajeDeDescuento);
+            }
+  }
+  console.log(producto.calcularPrecioDescuento)
